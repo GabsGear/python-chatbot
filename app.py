75f3cb567b30daf0a7c20b190b345480d320497c
gabsgear <gabriel_ghellere@hotmail.com>
2018-09-14 20:08:12 -0300

    documentacao

--- app.py
+++ app.py
@@ -1,31 +1,81 @@
+# -*- coding: utf-8 -*-
+""" 
+
+    Aplicação flask para integração com javascript do chatbot.
+
+    -Função:
+        Esse módulo processa o as requisições ajax e cria as rotas para a comunicação do backend 
+        (python) com o frontend (javasript).
+        
+        São criadas duas rotas, a default do index e a roda /send onde são enviadas as respostas
+        do bot para a interface.
+
+    -Autores: -Eduardo Vianna
+              -Gabriel Estevam
+              -Gabriel Ghellere
+
+"""
+
+# Importacao de bibliotecas
 from __future__ import print_function
+
+# -Modulos globais:
+#  flask 
 from flask import Flask, render_template, request
 from concurrent import futures
 import time
+
+# gprc protocol, da gprcio lib
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
+
+# Modulos chatterbot
 import chatterbot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
 import os
-import sys
-import re
-import json
 
+# -Modulos locais (api grpc)
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+# instancia a aplicacao no flask
 app = Flask(__name__)
+
+# cria a rota index
 @app.route('/')
+
 def index():
+    """
+        Objetivo: Essa funcao renderiza o template default do site, onde são carregadas todas
+        as informações estáticas a partir do template client.html localizado na pasta templates.
+
+        Quando executamos esse aplicativo a primeira página renderizada no servidor sera a pagina
+        definida na funcao render template (client.html no nosso caso).
+    """
     return render_template('client.html')
 
+# cria a rota /send e habilita o metodo POST
 @app.route('/send', methods = ['POST'])
+
 def get_post_javascript_data():
+    """
+        Objetivo: Essa funcao realiza as seguintes operacoes:
+            1- Recebe a mensagem do client  por meio de do metodo POST
+            2- Chama a rotina do servidor para processar a mensagem recebida
+            3- Recebe do servidor uma resposta
+            4- Retorna resposta recebida do servidor para o frontend
+    """
+    # instanciando canal de comunicacao grpc
     with grpc.insecure_channel('localhost:50051') as channel:
+        # recebe a mensagem do client por ajax
         msg = request.form['msg_cliente']
+        # obtem a resposta do chatbot chamando o servidor e retorna para o frontend
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name=msg))
     return str(response.message)
 
 if __name__ == "__main__":
+
+    # Levanta o servidor local e executa a aplicaçao
     app.run(debug=True)
 
--- greeter_server.py
+++ greeter_server.py
@@ -1,45 +1,92 @@
+# -*- coding: utf-8 -*-
+""" 
+    Servidor grpc e integracao com chatterbot.
+
+    -Função:
+        Nesse modulo é treinado o chatbot e integrado com a api chatterbot e feita toda a comunicação grpc
+
+    -Autores: -Eduardo Vianna
+              -Gabriel Estevam
+              -Gabriel Ghellere
+
+"""
+
+# Modulos GLobais
 from concurrent import futures
 import time
+import os
 
+# GRPC
 import grpc
 
-import helloworld_pb2
-import helloworld_pb2_grpc
-
-
+# Chatterbot
 import chatterbot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
-import os
 
+# Modulos locais
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+# instanciado periodo diario
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+# instancia um novo bot do modulo chatterbot
 bot = ChatBot("MitoBot")
+
+"""
+    Seleciona o tipo de treinamento do chatterbot, no nosso caso usamos um dataset treinado entao 
+    para treino online executamos ChatterBotCorpusTrainer no parametro set_trainer
+"""
 bot.set_trainer(ChatterBotCorpusTrainer)
 
+""" 
+    Seleciona o dataset de treino corpus, eh um dataset open sourse disponivel em https://github.com/gunthercox/chatterbot-corpus
+    Apos treinada a rede eh criado um banco de dados sqlite no path para que nao seje necessário retreino a cada novo uso
+"""
 bot.train(
     "chatterbot.corpus.portuguese"
 )
 
 class Chatbot():
 
-    def getResponse(self, message):
-        ## tem que transformar isso em string
+    def getResponse(self, message): 
+        """
+            Objetivo: Essa funcao recebe como argumento uma mensagem aleatória e retorna a resposta 
+            do bot. A mensagem enviada é selecionada pelo "match" da rede treinada que fica armazenado 
+            no arquivo db.sqlite3 criado no treinamento
+        """
         return str(bot.get_response(message.name))
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
-
+    """
+        A classe Greeter eh criada ultilizando heranca da classe GreeterServicer para assim poder utilizar as funcoes ja criadas da api
+    """
     def SayHello(self, request, context):
+
+        # cria um objeto da classe chatbot
         response = Chatbot()
+        # obtem a resposta do bot chamando getResponse
         response = response.getResponse(request)
+        # retorna a mensagem no padrao grpc
         return helloworld_pb2.HelloReply(message=response)
 
 
 def serve():
+    """
+        OBjetivo: Cria o servidor de comunicacao grpc na porta 50051
+    """
+
+    #define o maximo de threads na pool no caso 10
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # linka o servidor a classe gretter
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    # define a porta para conexao com o client
     server.add_insecure_port('[::]:50051')
+    #inicia o server
     server.start()
+    
+    # Recuperacao de erro
     try:
         while True:
             time.sleep(_ONE_DAY_IN_SECONDS)
