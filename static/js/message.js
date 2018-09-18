352d8d8acf78dfda7ca2f496950341b9985d33b0
edu.viana <eduardocosta_viana@live.com>
2018-09-18 12:31:35 -0300

    Documentação Código

--- static/js/message.js
+++ static/js/message.js
@@ -1,7 +1,12 @@
-
+// Botão para dar refresh na página e assim começar uma nova conversa
 $(".btn-reset").click(function(){
     location.reload();
 })
+
+/**
+ * Função criada com o objetivo de adicionar a 
+ * mensagem no chat.
+ */
 
 (function () {
     var Message;
@@ -20,6 +25,15 @@ $(".btn-reset").click(function(){
         }(this);
         return this;
     };
+
+    /**
+     * Funcão com o objetivo de obter a mensagem, tratar ela
+     *  e então encaminhar para a função responsavel por enviar
+     *  a mensagem ao chat. 
+     * 
+     * getMessageText() - Tem como objetivo obter a mensagem do usuário
+     * sendMessage() - Tem como objetivo exibir a mensagem no chat.
+     */
     $(function () {
         var getMessageText, message_side, sendMessage;
         message_side = 'right';
@@ -43,9 +57,17 @@ $(".btn-reset").click(function(){
             message.draw();
             return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
         };
+
+        //Função que enviará as mensagens ao chat quando clicado no botão de Enviar
         $('.send_message').click(function (e) {
             var text = getMessageText()
             sendMessage(getMessageText())
+                
+            /**
+            * Envia a mensagem do usuário via ajax e POST, para que 
+            * o servidor possa tratar e enviar essa mensagem para o c
+            * hatterbot e assim obter uma resposta.
+            */
             $.ajax({                
                 url: '/send',
                 data: {msg_cliente : text},
@@ -54,6 +76,8 @@ $(".btn-reset").click(function(){
                 error:function(error){console.log(error)}
             });
         });
+
+        //Função que enviará as mensagens ao chat quando presionado o botão ENTER
         $('.message_input').keyup(function (e) {
             if (e.which === 13) {
                 var text = getMessageText()
